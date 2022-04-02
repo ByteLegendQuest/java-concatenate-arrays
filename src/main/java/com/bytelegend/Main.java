1cbe0f0f04c99842ccb07d74a2f3bbfdfcc1dc57
hupizhu <2815695640@qq.com>
2022-04-02 10:31:47 +0000

    Update at 2022-04-02T10:31:47.934284Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -11,7 +11,9 @@ public class Main {
     }
 
     public static Object[] concatenate(Object[] a, Object[] b) {
-        System.arraycopy(b, 0, a, a.length - 1, b.length);
-        return a;
+        Object[] c = new Object[a.length + b.length];
+        System.arraycopy(a, 0, c, 0, a.length);
+        System.arraycopy(b, 0, c, a.length, b.length);
+        return c;
     }
 }
