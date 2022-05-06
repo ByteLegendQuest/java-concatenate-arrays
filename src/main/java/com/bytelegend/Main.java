ed613be9ad2f45ec98c3bf95bb9cb1e15f2b37d6
michealmachine <micheal0machine@gmail.com>
2022-05-06 10:58:21 +0000

    Update at 2022-05-06T10:58:21.171722Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -5,10 +5,17 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         System.out.println(
-                Arrays.toString(concatenate(new String[] {"123"}, new String[] {"456"})));
+                Arrays.toString(concatenate(new String[]{"123"}, new String[]{"456"})));
         System.out.println(
-                Arrays.toString(concatenate(new Object[] {1, 2.0}, new Object[] {"3", "4", "5"})));
+                Arrays.toString(concatenate(new Object[]{1, 2.0}, new Object[]{"3", "4", "5"})));
     }
 
-    public static Object[] concatenate(Object[] a, Object[] b) {}
+    public static Object[] concatenate(Object[] a, Object[] b) {
+        int x = a.length;
+        int y = b.length;
+        Object[] sc = new Object[x + y];
+        System.arraycopy(a, 0, sc, 0, x);
+        System.arraycopy(b, 0, sc, x, y);
+        return sc;
+    }
 }
