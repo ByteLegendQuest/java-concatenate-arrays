3821e554a79d8b3ae6b4f5b4dea36daf7df83b3c
TaoYang1998 <1121758475@qq.com>
2022-08-01 13:33:11 +0000

    Update at 2022-08-01T13:33:11.549964Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -5,10 +5,22 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         System.out.println(
-                Arrays.toString(concatenate(new String[] {"123"}, new String[] {"456"})));
+                Arrays.toString(concatenate(new String[]{"123"}, new String[]{"456"})));
         System.out.println(
-                Arrays.toString(concatenate(new Object[] {1, 2.0}, new Object[] {"3", "4", "5"})));
+                Arrays.toString(concatenate(new Object[]{1, 2.0}, new Object[]{"3", "4", "5"})));
     }
 
-    public static Object[] concatenate(Object[] a, Object[] b) {}
+    public static Object[] concatenate(Object[] a, Object[] b) {
+        Object[] c = new Object[a.length + b.length];
+
+        for (int i = 0; i <= a.length - 1; i++) {
+            c[i] = a[i];
+        }
+
+        for (int i = 0; i <= b.length - 1; i++) {
+            c[a.length + i] = b[i];
+        }
+
+        return c;
+    }
 }
