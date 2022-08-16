f416fa8b0399d0302a75a8859c52f285eda936cf
agirnob <kakcayigitbaran@gmail.com>
2022-08-16 19:33:45 +0000

    Update at 2022-08-16T19:33:45.011943Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -10,5 +10,15 @@ public class Main {
                 Arrays.toString(concatenate(new Object[] {1, 2.0}, new Object[] {"3", "4", "5"})));
     }
 
-    public static Object[] concatenate(Object[] a, Object[] b) {}
+    public static Object[] concatenate(Object[] a, Object[] b) {
+
+        List<Object> resultlist = new ArrayList<>(a.length + b.length);
+        Collections.addAll(resultlist, a);
+        Collections.addAll(resultlist, b);
+
+        @SuppressWarnings("unchecked")
+        //the type cast is safe as the array1 has the type T[]
+        Object[] resultArray = (Object[]) Array.newInstance(a.getClass().getComponentType(), 0);
+        return resultlist.toArray(resultArray);
+    }
 }
