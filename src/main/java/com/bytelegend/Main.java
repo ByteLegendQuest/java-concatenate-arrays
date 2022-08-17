15c667c7f8007fc26ce6f8407bd041c7a07285d8
shenshaoming <626546063@qq.com>
2022-08-17 02:58:04 +0000

    Update at 2022-08-17T02:58:04.108141Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -12,8 +12,8 @@ public class Main {
 
     public static Object[] concatenate(Object[] a, Object[] b) {
         Object[] c = new Object[a.length + b.length];
-        System.arrayCopy(a, 0, c, 0, a.length);
-        System.arrayCopy(b, 0, c, a.length, b.length);
+        System.arraycopy(a, 0, c, 0, a.length);
+        System.arraycopy(b, 0, c, a.length, b.length);
         return c;
     }
 }
