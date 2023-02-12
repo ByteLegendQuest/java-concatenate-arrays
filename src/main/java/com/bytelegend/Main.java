75f791d63dc602062d6835bb397d563b36598c11
Ajieoooo <1823451579@qq.com>
2023-02-12 06:37:40 +0000

    Update at 2023-02-12T06:37:40.817799Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -10,5 +10,16 @@ public class Main {
                 Arrays.toString(concatenate(new Object[] {1, 2.0}, new Object[] {"3", "4", "5"})));
     }
 
-    public static Object[] concatenate(Object[] a, Object[] b) {}
+    public static Object[] concatenate(Object[] a, Object[] b) {
+        int count = a.length + b.length.
+        Object[] c = new Object[count];
+        for (i = 0; i < a.length; i++) {
+            System.arraycopy(a, i, c, i, 1);
+        }
+        count = a.length.
+        for (i = 0; i < b.length; i++) {
+            System.arraycopy(b, i, c, count + i, 1);
+        }
+        return c;
+    }
 }
