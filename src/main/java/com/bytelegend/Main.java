8e134395229cbaed73ae06e755d5352c77686734
vinny59200 <vvauban@gmail.com>
2023-05-25 01:05:39 +0000

    Update at 2023-05-25T01:05:39.485201Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -10,5 +10,12 @@ public class Main {
                 Arrays.toString(concatenate(new Object[] {1, 2.0}, new Object[] {"3", "4", "5"})));
     }
 
-    public static Object[] concatenate(Object[] a, Object[] b) {}
+    public static Object[] concatenate( Object[] a, Object[] b ) {
+        Object[] result = new Object[a.length + b.length];
+        System.arraycopy( a, 0, result, 0, a.length );
+
+        // Copy elements from array2 to the result array
+        System.arraycopy( b, 0, result, a.length, b.length );
+        return result;
+    }
 }
